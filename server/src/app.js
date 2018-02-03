89cc5e90bba9f4798eb29fa2c2d0b0f5e64c4b3e
Ademir Besirovic <ademir.besirovic1996@gmail.com>
2018-02-03 15:30:43 +0100

    Setup express server

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+const PORT = process.env.PORT || 8000;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/status', (req, res, next) => {
+    res.send({
+        message: 'Hello world'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is runing on port ${PORT}`);
+});
